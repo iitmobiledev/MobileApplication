dc6185c5ab9ea0af3ce6caeed7e038658dcc2b94
Алексей Кочев <kochev.alexey@gmail.com>
2014-07-03 22:57:09 +0600

    Work with angularjs service
    Test work with service

--- App/www/script.js
+++ App/www/script.js
@@ -1,34 +1,44 @@
-function MyCtrl($scope) {
-    $scope.action = function() {
-        if ($scope.name == 'Вчера')
-        {
+angular.module('myService', []).
+  factory('notify', function($window) {
+    return function(msg) {
+        $window.alert(msg);
+    };
+  }).
+  controller('MyController', function($scope, notify) 
+{
+     $scope.action = function () {
+        if ($scope.name == 'Вчера') {
             $scope.name = 'Сегодня';
             $scope.classButtonNext = 'buttonHide';
-        }
-        else //if ($scope.name == 'Позавчера')
+        } else //if ($scope.name == 'Позавчера')
         {
             $scope.name = 'Вчера';
             $scope.classButtonNext = 'button widget uib_w_8 smallNavigationButton d-margins icon right';
             $scope.classButtonPrev = 'button widget uib_w_8 smallNavigationButton d-margins icon left';
         }
     }
-    
+
     $scope.name = 'Вчера';
-    
-    $scope.reaction = function() {
-        if ($scope.name == 'Вчера')
-        {
+
+    $scope.reaction = function () {
+        if ($scope.name == 'Вчера') {
             $scope.name = 'Позавчера';
             $scope.classButtonPrev = 'buttonHide';
-        }
-        else //if ($scope.name == 'Сегодня')
+        } else //if ($scope.name == 'Сегодня')
         {
             $scope.name = 'Вчера';
             $scope.classButtonPrev = 'button widget uib_w_8 smallNavigationButton d-margins icon left';
             $scope.classButtonNext = 'button widget uib_w_8 smallNavigationButton d-margins icon right';
         }
     }
-    
+
     $scope.classButtonNext = 'button widget uib_w_8 smallNavigationButton d-margins icon right';
     $scope.classButtonPrev = 'button widget uib_w_8 smallNavigationButton d-margins icon left';
-}
+    
+    /////
+    $scope.callNotify = function(msg) {
+        
+        notify(msg);
+        
+    };
+  });
