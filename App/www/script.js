b56b0717469c06d63cb432a3a0bbc8fe63ef07f0
Алексей Кочев <kochev.alexey@gmail.com>
2014-07-04 10:45:51 +0600

    Хорошо работают кнопки навигации

--- App/www/script.js
+++ App/www/script.js
@@ -1,63 +1,39 @@
 angular.module('myService', []).
-  factory('notify', function($window) {
-    return function(msg) {
+factory('notify', function ($window) {
+    return function (msg) {
         $window.alert(msg);
     };
-  }).
-  controller('MyController', function($scope, notify) 
-{
-    $scope.date=new Date();
-     $scope.hasPreviousData=function(){
-         return true;
-     };
-    
-    $scope.forward=function(){
-        $scope.date.setDate($scope.date.getDate()+1);
+}).
+controller('MyController', function ($scope, notify) {
+    $scope.date = new Date();
+    $scope.hasPreviousData = function () {
+        return true;
+    };
+    $scope.hasFutureData = function () {
+        if ($scope.date.getDate() == new Date().getDate()) {
+            return false;
+        }
+        else{
+            return true;
+        }
+    };
+    $scope.forward = function () {
+        $scope.date.setDate($scope.date.getDate() + 1);
         $scope.action(); // remove                    
     };
-   $scope.back=function(){
-        $scope.date.setDate($scope.date.getDate()-1);
+    $scope.back = function () {
+        $scope.date.setDate($scope.date.getDate() - 1);
         $scope.reaction(); // remove                    
-    }
-    
-    $scope.getTitle = function(){
+    };
+
+    $scope.getTitle = function () {
         return $scope.date.toUTCString();
     }
 
-     $scope.action = function () {
-        if ($scope.name == 'Вчера') {
-            $scope.name = 'Сегодня';
-            $scope.classButtonNext = 'buttonHide';
-        } else //if ($scope.name == 'Позавчера')
-        {
-            $scope.name = 'Вчера';
-            $scope.classButtonNext = 'button widget uib_w_8 smallNavigationButton d-margins icon right';
-            $scope.classButtonPrev = 'button widget uib_w_8 smallNavigationButton d-margins icon left';
-        }
-    }
-     
-    
-    $scope.name = 'Вчера';
+    $scope.callNotify = function (msg) {
 
-    $scope.reaction = function () {
-        if ($scope.name == 'Вчера') {
-            $scope.name = 'Позавчера';
-            $scope.classButtonPrev = 'buttonHide';
-        } else //if ($scope.name == 'Сегодня')
-        {
-            $scope.name = 'Вчера';
-            $scope.classButtonPrev = 'button widget uib_w_8 smallNavigationButton d-margins icon left';
-            $scope.classButtonNext = 'button widget uib_w_8 smallNavigationButton d-margins icon right';
-        }
-    }
-
-    $scope.classButtonNext = 'button widget uib_w_8 smallNavigationButton d-margins icon right';
-    $scope.classButtonPrev = 'button widget uib_w_8 smallNavigationButton d-margins icon left';
-    
-    /////
-    $scope.callNotify = function(msg) {
-        
         notify(msg);
-        
+
     };
-  });
+    $scope.callNotify('ffff');
+});
