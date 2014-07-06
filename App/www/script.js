cec9753885f74cd6768d104e6968bbe0a31ed951
Алексей Кочев <kochev.alexey@gmail.com>
2014-07-06 20:40:11 +0600

    Обновление данных при нажатии кнопок навигации

--- App/www/script.js
+++ App/www/script.js
@@ -1,5 +1,3 @@
-
-
 angular.module('myService', []).
 factory('notify', function () {
     return function (startDay, endDay) {
@@ -7,38 +5,43 @@ factory('notify', function () {
         manyData = manyData.filter(function (d) {
             return (d.date.getDate() <= startDay.getDate() && d.date.getDate() >= endDay.getDate());
         });
-//        if ( manyData.length > 1)
-//        {
-//            throw new Error("Дате " + manyData[0].date.toDateString() + " соответствует > 1 объекта"); 
-//        }
-        return manyData;//[0];
+        //        if ( manyData.length > 1)
+        //        {
+        //            throw new Error("Дате " + manyData[0].date.toDateString() + " соответствует > 1 объекта"); 
+        //        }
+        return manyData;
     };
 }).
 controller('MyController', function ($scope, notify) {
     $scope.date = new Date();
+
     $scope.hasPreviousData = function () {
+        $scope.data = notify($scope.date, $scope.date);
         return true;
     };
+
     $scope.hasFutureData = function () {
         if ($scope.date.getDate() == new Date().getDate()) {
             return false;
         } else {
+            $scope.data = notify($scope.date, $scope.date);
             return true;
         }
     };
+
     $scope.forward = function () {
         $scope.date.setDate($scope.date.getDate() + 1);
-        $scope.data = notify($scope.date);          
+        $scope.data = notify($scope.date);
     };
     $scope.back = function () {
         $scope.date.setDate($scope.date.getDate() - 1);
-        $scope.data = notify($scope.date);                 
+        $scope.data = notify($scope.date);
     };
 
     $scope.getTitle = function () {
         return $scope.date.toUTCString();
     }
-    
-    $scope.data = notify($scope.date, $scope.date);// new Date($scope.date.getDate() - 7));
+
+    $scope.data = notify($scope.date, $scope.date); // new Date($scope.date.getDate() - 7));
 
 });
