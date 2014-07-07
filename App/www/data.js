701a6366c51c337795d062813d05041ac783e9ba
Mirann <annaibakaeva@google.com>
2014-07-07 11:39:56 +0600

    + data for week

--- App/www/data.js
+++ App/www/data.js
@@ -43,13 +43,7 @@ function getData() {
     manyData.push(new Data(new Date(2014, 6, 4), 5000, 7000, 15, 85, 3000, 1000, 500, -5400));
     manyData.push(new Data(new Date(2014, 6, 3), 7000, 500, 13, 60, 6000, 2000, 1700, +2100));
     manyData.push(new Data(new Date(2014, 6, 2), 100, 3500, 10, 45, 900, 3500, 4500, -10000));
-    //    manyData.push(new Data(new Date(2014, 6, 4), 2000, 2000, 5, 80, 5000, 3000, 2500, -500));
-    //    manyData.push(new Data(new Date(2014, 6, 3), 5000, 4000, 10, 50, 5000, 3000, 2500, -500));
-    //    manyData.push(new Data(new Date(2014, 6, 2), 3000, 1000, 9, 70, 5000, 3000, 2500, -500));
-    //    manyData.push(new Data(new Date(2014, 6, 1), 4000, 2000, 7, 80, 5000, 3000, 2500, -500));
-    //    manyData.push(new Data(new Date(2014, 5, 30), 5000, 3000, 11, 50, 5000, 3000, 2500, -500));
-    //    manyData.push(new Data(new Date(2014, 5, 29), 2500, 2000, 6, 50, 5000, 3000, 2500, -500));
-    //    manyData.push(new Data(new Date(2014, 5, 28), 1000, 1000, 5, 50, 5000, 3000, 2500, -500));
-    //    manyData.push(new Data(new Date(2014, 6, 6), 1000, 3000, 12, 70, 5000, 3000, 2500, -500));
+    manyData.push(new Data(new Date(2014, 6, 7), 100, 3500, 10, 45, 900, 3500, 4500, -10000));
+    manyData.push(new Data(new Date(2014, 6, 1), 1000, 3500, 10, 45, 900, 3500, 4500, -1000));
     return manyData;
 }
--- App/www/script.js
+++ App/www/script.js
@@ -1,3 +1,22 @@
+function getSumDataFromArray(dataArray){
+    var date, proceeds=0, profit=0, clients=0, workload=0, tillMoney=0, morningMoney=0, credit=0, debit=0;
+    for (var i =0; i < dataArray.length; i++)
+    {
+        date = dataArray[i].date;
+        proceeds += dataArray[i].proceeds;
+        profit += dataArray[i].profit;
+        clients += dataArray[i].clients;
+        workload += dataArray[i].workload;
+        tillMoney += dataArray[i].tillMoney;
+        morningMoney += dataArray[i].morningMoney;
+        credit += dataArray[i].credit;
+        debit += dataArray[i].debit;
+    }
+    workload = Math.round(workload/dataArray.length);
+    return new Data(date, proceeds, profit, clients, workload, tillMoney, morningMoney, credit, debit);
+}
+
+
 angular.module('myService', []).
 factory('notify', function () {
     return function (startDay, endDay) {
@@ -5,43 +24,52 @@ factory('notify', function () {
         manyData = manyData.filter(function (d) {
             return (d.date.getDate() <= startDay.getDate() && d.date.getDate() >= endDay.getDate());
         });
-        //        if ( manyData.length > 1)
-        //        {
-        //            throw new Error("Дате " + manyData[0].date.toDateString() + " соответствует > 1 объекта"); 
-        //        }
         return manyData;
     };
 }).
 controller('MyController', function ($scope, notify) {
     $scope.date = new Date();
-
+    var endDay = $scope.date;
     $scope.hasPreviousData = function () {
-        $scope.data = notify($scope.date, $scope.date);
         return true;
     };
-
     $scope.hasFutureData = function () {
         if ($scope.date.getDate() == new Date().getDate()) {
             return false;
         } else {
-            $scope.data = notify($scope.date, $scope.date);
             return true;
         }
     };
-
     $scope.forward = function () {
-        $scope.date.setDate($scope.date.getDate() + 1);
-        $scope.data = notify($scope.date);
+        $scope.date.setDate($scope.date.getDate() + 1);   
+        endDay = $scope.date;
+        $scope.data = getSumDataFromArray(notify($scope.date, $scope.date));
     };
     $scope.back = function () {
         $scope.date.setDate($scope.date.getDate() - 1);
-        $scope.data = notify($scope.date);
+        endDay = $scope.date;
+        $scope.data = getSumDataFromArray(notify($scope.date, $scope.date));
     };
 
     $scope.getTitle = function () {
-        return $scope.date.toUTCString();
+        if ($scope.date.getDate() == endDay.getDate())
+            return $scope.date.toUTCString();
+        return $scope.date.toUTCString() + " - " + endDay.toUTCString();
+    };
+    
+    $scope.forDay = function() {
+        endDay = $scope.date;
+        $scope.data = getSumDataFromArray(notify($scope.date, endDay));
     }
-
-    $scope.data = notify($scope.date, $scope.date); // new Date($scope.date.getDate() - 7));
-
+    $scope.forWeek = function() {
+        endDay = new Date($scope.date.getDate() - 7);
+        $scope.data = getSumDataFromArray(notify($scope.date, endDay));
+    }
+    $scope.forMonth = function() {
+        endDay = new Date($scope.date.getFullYear(), $scope.date.getMonth() - 1, $scope.date.getDay());
+        $scope.data = getSumDataFromArray(notify($scope.date, endDay));
+    }
+    
+    //var dataArray = notify($scope.date, $scope.date);// new Date($scope.date.getDate() - 7));
+    $scope.data = getSumDataFromArray(notify($scope.date, endDay));
 });
