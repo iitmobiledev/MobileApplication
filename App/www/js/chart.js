3d20430dedc0332370c2e293b4a46f0fdb0f1fdb
Danil <yusupdan@gmail.com>
2014-07-08 11:54:21 +0600

    Change chart

--- App/www/js/chart.js
+++ App/www/js/chart.js
@@ -1,68 +1,75 @@
-function getRandom(min, max)
-{
-  return Math.random() * (max - min) + min;
-}
+var a = [
+                    [Date.UTC(1970,  9, 27), 0   ],
+                    [Date.UTC(1970, 10, 10), 0.6 ],
+                    [Date.UTC(1970, 10, 18), 0.7 ],
+                    [Date.UTC(1970, 11,  2), 0.8 ],
+                    [Date.UTC(1970, 11,  9), 0.6 ],
+                    [Date.UTC(1970, 11, 16), 0.6 ],
+                    [Date.UTC(1970, 11, 28), 0.67],
+                    [Date.UTC(1971,  0,  1), 0.81],
+                    [Date.UTC(1971,  0,  8), 0.78],
+                    [Date.UTC(1971,  0, 12), 0.98],
+                    [Date.UTC(1971,  0, 27), 1.84],
+                    [Date.UTC(1971,  1, 10), 1.80],
+                    [Date.UTC(1971,  1, 18), 1.80],
+                    [Date.UTC(1971,  1, 24), 1.92],
+                    [Date.UTC(1971,  2,  4), 2.49],
+                    [Date.UTC(1971,  2, 11), 2.79],
+                    [Date.UTC(1971,  2, 15), 2.73],
+                    [Date.UTC(1971,  2, 25), 2.61],
+                    [Date.UTC(1971,  3,  2), 2.76],
+                    [Date.UTC(1971,  3,  6), 2.82],
+                    [Date.UTC(1971,  3, 13), 2.8 ],
+                    [Date.UTC(1971,  4,  3), 2.1 ],
+                    [Date.UTC(1971,  4, 26), 1.1 ],
+                    [Date.UTC(1971,  5,  9), 0.25],
+                    [Date.UTC(1971,  5, 12), 0   ]
+]
 
 $(function () {
-        var a = [];
-        for (var i = 0; i < 365; i++)
-        {
-            a.push(getRandom(1000, 50000));
-        }
+    Highcharts.setOptions({
+	lang: {
+		months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',  'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        shortMonths: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн',  'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+		weekdays: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
+	}
+});
+    showChart(a);
+});
+    
+function showChart(dataList)
+{
         $('#container').highcharts({
             chart: {
+                type: 'spline',
                 zoomType: 'x'
             },
             title: {
                 text: ''
             },
-            subtitle: {
-                text: document.ontouchstart === undefined ?
-                    'Click and drag in the plot area to zoom in' :
-                    'Pinch the chart to zoom in'
-            },
             xAxis: {
                 type: 'datetime',
-                minRange: 14 * 24 * 3600000 // fourteen days
+                minRange: 14 * 24 * 3600000, // fourteen days
+                dateTimeLabelFormats: { // don't display the dummy year
+                    
+                    month: '%e %b %y'
+                },
             },
             yAxis: {
                 title: {
-                    text: 'Exchange rate'
-                }
+                    text: ''
+                },
+            },
+            tooltip: {
+                headerFormat: '',
+                pointFormat: '{point.x:%e. %b}: {point.y:.2f}р.'
             },
             legend: {
                 enabled: false
             },
-            plotOptions: {
-                area: {
-                    fillColor: {
-                        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
-                        stops: [
-                            [0, Highcharts.getOptions().colors[0]],
-                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                        ]
-                    },
-                    marker: {
-                        radius: 2
-                    },
-                    lineWidth: 1,
-                    states: {
-                        hover: {
-                            lineWidth: 1
-                        }
-                    },
-                    threshold: null
-                }
-            },
-    
             series: [{
-                type: 'area',
-                name: 'USD to EUR',
-                pointInterval: 24 * 3600 * 1000,
-                pointStart: Date.UTC(2006, 0, 01),
-                data: a
+                name: '',
+                data: dataList
             }]
         });
-    });
-    
-
+    }
