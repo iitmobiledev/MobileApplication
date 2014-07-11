f0e5f29a32ab7b6baca171fa98520f8f6c2f1019
Danil <yusupdan@gmail.com>
2014-07-11 12:42:30 +0600

    random data generation

--- App/www/data.js
+++ App/www/data.js
@@ -35,16 +35,16 @@ function Record(client, serviceList, comment, date) {
 
 //Клиент
 function Client(firstName, middleName, lastName, phoneNumber, balance, discount) {
-    this.firstName = firstName;//имя
-    this.middleName = middleName;// отчество
-    this.lastName = lastName;//фамилия
+    this.firstName = firstName; //имя
+    this.middleName = middleName; // отчество
+    this.lastName = lastName; //фамилия
     this.phoneNumber = phoneNumber; //номер телефона
     this.balance = balance; //баланс
     this.discount = discount; // скидка
 }
 
 //Услуга
-function Service(description, startTime, endTime, master, cost){
+function Service(description, startTime, endTime, master, cost) {
     this.description = description; //название услуги
     this.startTime = startTime; //время начала оказания услуги
     this.endTime = endTime; //время конца оказания услуги
@@ -53,33 +53,48 @@ function Service(description, startTime, endTime, master, cost){
 }
 
 //мастер
-function Master(firstName, middleName, lastName){
-    this.firstName = firstName;//имя
-    this.middleName = middleName;// отчество
-    this.lastName = lastName;//фамилия
+function Master(firstName, middleName, lastName) {
+    this.firstName = firstName; //имя
+    this.middleName = middleName; // отчество
+    this.lastName = lastName; //фамилия
 }
 
 //Пользователь
-function User(firstName, middleName, lastName, email){
-    this.firstName = firstName;//имя
-    this.middleName = middleName;// отчество
-    this.lastName = lastName;//фамилия
-    this.email = email;//почта
+function User(firstName, middleName, lastName, email) {
+    this.firstName = firstName; //имя
+    this.middleName = middleName; // отчество
+    this.lastName = lastName; //фамилия
+    this.email = email; //почта
 }
 
 function getData() {
     var manyData = [];
-    manyData.push(new OperationalStatistics(new Date(2013, 11, 30), 3300, 1000, 5, 80, 5000, 10000, 2500, -300));
-    manyData.push(new OperationalStatistics(new Date(2014, 3, 10), 3300, 1000, 5, 80, 5000, 10000, 2500, -300));
-    manyData.push(new OperationalStatistics(new Date(2014, 6, 8), 3300, 1000, 5, 80, 5000, 10000, 2500, -300));
-    manyData.push(new OperationalStatistics(new Date(2014, 6, 6), 1000, 3000, 12, 70, 5000, 3000, 2500, -500));
-    manyData.push(new OperationalStatistics(new Date(2014, 6, 5), 2000, 1000, 20, 15, 7000, 9000, 1500, +5050));
-    manyData.push(new OperationalStatistics(new Date(2014, 6, 4), 5000, 7000, 15, 85, 3000, 1000, 500, -5400));
-    manyData.push(new OperationalStatistics(new Date(2014, 6, 3), 2500, 500, 13, 60, 6000, 2000, 1700, +2100));
-    manyData.push(new OperationalStatistics(new Date(2014, 6, 2), 4444, 3500, 10, 45, 900, 3500, 4500, -10000));
-    manyData.push(new OperationalStatistics(new Date(2014, 6, 7), 5555, 3500, 10, 45, 900, 3500, 4500, -10000));
-    manyData.push(new OperationalStatistics(new Date(2014, 6, 1), 10000, 3500, 10, 45, 900, 3500, 4500, -1000));
+    var nowDay = new Date();
+    //    manyData.push(new OperationalStatistics(new Date(2013, 11, 30), 3300, 1000, 5, 80, 5000, 10000, 2500, -300));
+    //    manyData.push(new OperationalStatistics(new Date(2014, 3, 10), 3300, 1000, 5, 80, 5000, 10000, 2500, -300));
+    //    manyData.push(new OperationalStatistics(new Date(2014, 6, 8), 3300, 1000, 5, 80, 5000, 10000, 2500, -300));
+    //    manyData.push(new OperationalStatistics(new Date(2014, 6, 6), 1000, 3000, 12, 70, 5000, 3000, 2500, -500));
+    //    manyData.push(new OperationalStatistics(new Date(2014, 6, 5), 2000, 1000, 20, 15, 7000, 9000, 1500, +5050));
+    //    manyData.push(new OperationalStatistics(new Date(2014, 6, 4), 5000, 7000, 15, 85, 3000, 1000, 500, -5400));
+    //    manyData.push(new OperationalStatistics(new Date(2014, 6, 3), 2500, 500, 13, 60, 6000, 2000, 1700, +2100));
+    //    manyData.push(new OperationalStatistics(new Date(2014, 6, 2), 4444, 3500, 10, 45, 900, 3500, 4500, -10000));
+    //    manyData.push(new OperationalStatistics(new Date(2014, 6, 7), 5555, 3500, 10, 45, 900, 3500, 4500, -10000));
+    //    manyData.push(new OperationalStatistics(new Date(2014, 6, 1), 10000, 3500, 10, 45, 900, 3500, 4500, -1000));
+
+    
+    for (var i = 0; i < 300; i++) {
+        var a = getRandom(1000, 10000);
+        manyData.push(new OperationalStatistics(new Date(nowDay.getFullYear(), nowDay.getMonth(), nowDay.getDate() - i),
+                a,
+                a - getRandom(200, 5000),
+                getRandom(3, 50),
+                getRandom(50, 100)));
+    }
     return manyData.sort();
+}
+
+function getRandom(min, max) {
+    return Math.round(Math.random() * (max - min) + min) / 100;
 }
 
 
@@ -87,14 +102,13 @@ function getFinanceStatistics() {
     return new FinanceStatistics(13000, 1000, 5000, -2000);
 }
 
-function getExpenditures()
-{
+function getExpenditures() {
     var expList = [];
     var expItemsList = [];
     expItemsList.push(new ExpenditureItem("Покупка расходных материалов", -1500));
     expItemsList.push(new ExpenditureItem("Выплата дворнику", -500));
     expList.push(new Expenditures(new Date(2014, 6, 9), expItemsList));
-    
+
     expItemsList = [];
     expItemsList.push(new ExpenditureItem("Покупка расходных материалов", -1500));
     expItemsList.push(new ExpenditureItem("Покупка нового кресла", -5000));
@@ -102,6 +116,6 @@ function getExpenditures()
     return expList;
 }
 
-function getCurrentUser(){
+function getCurrentUser() {
     return new User("Анна", "Петровна", "Касатникова", "kasatnik@gmail.com");
 }
