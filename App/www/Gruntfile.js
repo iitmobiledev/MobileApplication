48986dc3fbc3580c53425c3cb81ccb7b46ff6886
Danil <yusupdan@gmail.com>
2014-07-17 13:24:36 +0600

    grunt

---
+++ App/www/Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        clean: ['doc'],
+        jsdoc: {
+            basic: {
+                src: ['js/*.js'],
+                options: {
+                    destination: 'doc'
+                }
+            },
+            //            docstrap: {
+            //                src: ['js/**.js'],
+            //                options: {
+            //                    destination: 'doc/docstrap',
+            //                    template: "node_modules/ink-docstrap/template",
+            //                    configure: "node_modules/ink-docstrap/template/jsdoc.conf.json"
+            //                }
+            //            }
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'js/**.js', 'js/directives/*.js', 'js/controllers/*.js'],
+            options: {
+                node: true,
+                smarttabs: true
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jsdoc');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('default', ['jshint', 'clean', 'jsdoc']);
+};
