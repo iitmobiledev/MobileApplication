9d0ca58a98a0aae1ea214603678f811ef7a4fce2
Mirann <annaibakaeva@google.com>
2014-07-18 14:47:02 +0600

    Документация

--- App/www/js/controllers/settings-controller.js
+++ App/www/js/controllers/settings-controller.js
@@ -1,7 +1,18 @@
- myApp.controller('SettingsController', function ($scope, UserLoader) {
+/**
+ * @ngdoc controller
+ * @name myApp.controller:SettingsController
+ * @description <p> Контроллер, отвечающий за загрузку данных о текущем
+ * пользователе.</p>
+ * <p>Переменные в scope:<br>
+ * {User} user текущий пользователь,<br>
+ * {Function} exit функция для выхода пользоателя из системы и
+ * перехода на страницу авторизации.
+ * </p>
+ */
+myApp.controller('SettingsController', function ($scope, UserLoader) {
      $scope.user = UserLoader();
      $scope.exit = function(){
-         alert("closing");
-         window.close();
+         // log out
+         //переход на страницу авторизации
      };
  });
