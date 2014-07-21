27c37b2e11cc73b073850d989fe224b396dad258
Danil <yusupdan@gmail.com>
2014-07-21 17:00:01 +0600

    some changes in visit

--- App/www/js/controllers/visit.js
+++ App/www/js/controllers/visit.js
@@ -17,12 +17,34 @@ myApp.controller('VisitController', function ($scope, $route, $routeParams) {
  * @name myApp.directive:visitPageContent
  * @restrict E
  */
-myApp.directive('visitPageContent', function () {
+myApp.directive('visitPageContent', function ($filter) {
     return {
         restrict: 'E',
         replace: true,
         transclude: false,
         link: function (scope, element, attrs) {
-            $(element).append('<div style="text-align: /"center/"">Запись на ' + scope.date.getHours() + ":" + scope.date.getMinutes() +'</div>')
+            $(element).append('<div style="text-align: center; background-color: yellow;">' + 
+                              'Запись на ' +
+                              $filter('date')(date, "H:mm dd.MM.yyyy"); +'</div>');
+            
+            var balanceColor;
+            var balance = scope.client.balance;
+            if(balance >= 0)
+                balanceColor = "red";
+            else
+                balanceColor = "green"
+                
+            $(element).append('<div>' + scope.client.lastName + " " + scope.client.firstName + " " + scope.client.middleName +
+                              '<br>' + 
+                              '<a style="text-decoration: none;" href="tel:' + 
+                              scope.client.phoneNumber + '">' + scope.client.phoneNumber + '</a>' +
+                              '<br>' +
+                              '<span style="color: ' + balanceColor + ';">Баланс: ' + balance + '</span>' + 
+                              '   Скидка: ' + scope.client.discount + '%' +
+                              '</div>');
+            
+            
+            $(element).append('<div>Услуги</div>')
+            
         },
         template: '<div></div>'
