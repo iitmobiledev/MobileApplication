b71d21bf0230b50fda2fbb29b059298a6dba57e2
Danil <yusupdan@gmail.com>
2014-07-21 14:13:23 +0600

    doc & start doing visit

--- App/www/js/controllers/visit.js
+++ App/www/js/controllers/visit.js
@@ -1,3 +1,25 @@
-//контроллер  страницы визита
-myApp.controller('VisitController', function ($scope) { 
+/**
+ * @description Контроллер, содержащий данные о визите. Получает данные из пути к странице
+ * @ngdoc controller
+ * @name myApp.controller:VisitController
+ * @param {Visit} visit Визит
+ */
+myApp.controller('VisitController', function ($scope, $route, $routeParams) { 
+    $scope.visit = $routeParams.visit;
  });
+
+/**
+ * @description Директива добавляет на страницу информацию о визите
+ * @ngdoc directive
+ * @name myApp.directive:visitPageContent
+ * @restrict E
+ */
+myApp.directive('visitPageContent', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        transclude: false,
+        link: function (scope, element, attrs) {
+            
+        },
+        templateUrl:  
