fae3ac38bcc04778995ea5cf2016c49f259b92e3
Mirann <annaibakaeva@google.com>
2014-08-08 11:07:40 +0600

    Loader без if

--- App/www/js/services/loader.js
+++ App/www/js/services/loader.js
@@ -1,32 +1,31 @@
-myApp.service("Loader", ["$http", "OperationalStatisticsData", "GetOpStatObjects", "DateHelper", function($http, OperationalStatisticsData, GetOpStatObjects, DateHelper){
-    return {
-        get: function(modelClass, primaryKey, callback){
-            //получили нужные данные
-            //преобразовали их в объекты
-            if (modelClass == "OperationalStatistics"){
-                var statData=OperationalStatisticsData(primaryKey.dateFrom, primaryKey.dateTill, primaryKey.step);
+myApp.service("Loader", ["$http", "OperationalStatisticsData", "GetOpStatObjects", "DateHelper",
+    function ($http, OperationalStatisticsData, GetOpStatObjects, DateHelper) {
+        return {
+            get: function (modelClass, primaryKey, callback) {
+                var loaders = {
+                    "OperationalStatistics": OperationalStatisticsData
+                };
+                //получили нужные данные
+                //преобразовали их в объекты
+                var statData = loaders[modelClass](primaryKey.dateFrom, primaryKey.dateTill, primaryKey.step);
                 var statObjs = GetOpStatObjects(statData);
                 if (statObjs.length == 1 && primaryKey.dateFrom.toDateString() == primaryKey.dateTill.toDateString())
                     statObjs = statObjs[0];
+
                 //тут должна быть запись в хранилище
                 //вместо return
                 return statObjs;
-                
+
                 //callback(statObjs);
+            },
+            search: function (className, primaryKey, callback) {
+                //пытаемся найти объект в хранилище
+                // если не нашли,то вызываем get()
+                //снова обращается к хранилище, 
+                //если данных вновь нет, то
+                // возвращаем null
+                var objs = this.get(className, primaryKey);
+                callback(objs);
             }
-            
-            
-        },
-        search: function(className, primaryKey, callback){
-            //пытаемся найти объект в хранилище
-            // если не нашли,то вызываем get()
-            //снова обращается к хранилище, 
-            //если данных вновь нет, то
-            // возвращаем null
-            var objs = this.get(className, primaryKey);
-            callback(objs);
         }
-    }
 }]);
-
-
