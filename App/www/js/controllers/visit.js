3ad0c215aeaa882968926d900a8b1e5dd27c5a04
Алексей Кочев <kochev.alexey@gmail.com>
2014-08-21 13:00:51 +0600

    visit

--- App/www/js/controllers/visit.js
+++ App/www/js/controllers/visit.js
@@ -19,38 +19,38 @@ myApp.controller('VisitController', function ($scope, $filter, $routeParams, Sto
      * @description Загружает в переменные `$scope` всю информацию о
      * визите.
      */
-    $scope.showVisit = function () {
-        console.log("showVisit()-Visit:", $scope.visit);
-        if ($scope.visit) {
-            $scope.status = $scope.visit.status;
-            $scope.date = $scope.visit.date;
-            $scope.clientName = $scope.visit.client.lastName + " " + $scope.visit.client.firstName + " " + $scope.visit.client.middleName;
-            $scope.clientPhoneNumber = $scope.visit.client.phoneNumber;
-            $scope.comment = $scope.visit.comment;
-            $scope.balColor = "red";
-            if ($scope.visit.client.balance >= 0) {
-                $scope.balColor = "green";
-            }
-            $scope.clientBalance = $scope.visit.client.balance;
-            $scope.clientDiscount = $scope.visit.client.discount + "%"
-
-            $scope.servList = [];
-            $scope.sum = 0;
-            for (var i = 0; i < $scope.visit.serviceList.length; i++) {
-                var service = $scope.visit.serviceList[i];
-                $scope.sum += $scope.visit.serviceList[i].cost;
-                var serviceItem = {};
-                serviceItem.description = service.description;
-                serviceItem.time = $filter('date')(service.startTime, "H:mm") + " - " + $filter('date')(service.endTime, "H:mm");
-                serviceItem.cost = service.cost;
-                serviceItem.master = 'Мастер: ' + service.master.lastName + " " + service.master.firstName[0];
-                $scope.servList.push(serviceItem);
-            }
-            $scope.comment = $scope.visit.comment;
-        }
-    };
-
-    $scope.$watch('visit', function () {
-        $scope.showVisit();
-    });
+    //    $scope.showVisit = function () {
+    //        console.log("showVisit()-Visit:", $scope.visit);
+    //        if ($scope.visit) {
+    //            $scope.status = $scope.visit.status;
+    //            $scope.date = $scope.visit.date;
+    //            $scope.clientName = $scope.visit.client.lastName + " " + $scope.visit.client.firstName + " " + $scope.visit.client.middleName;
+    //            $scope.clientPhoneNumber = $scope.visit.client.phoneNumber;
+    //            $scope.comment = $scope.visit.comment;
+    //            $scope.balColor = "red";
+    //            if ($scope.visit.client.balance >= 0) {
+    //                $scope.balColor = "green";
+    //            }
+    //            $scope.clientBalance = $scope.visit.client.balance;
+    //            $scope.clientDiscount = $scope.visit.client.discount + "%"
+    //
+    //            $scope.servList = [];
+    //            $scope.sum = 0;
+    //            for (var i = 0; i < $scope.visit.serviceList.length; i++) {
+    //                var service = $scope.visit.serviceList[i];
+    //                $scope.sum += $scope.visit.serviceList[i].cost;
+    //                var serviceItem = {};
+    //                serviceItem.description = service.description;
+    //                serviceItem.time = $filter('date')(service.startTime, "H:mm") + " - " + $filter('date')(service.endTime, "H:mm");
+    //                serviceItem.cost = service.cost;
+    //                serviceItem.master = 'Мастер: ' + service.master.lastName + " " + service.master.firstName[0];
+    //                $scope.servList.push(serviceItem);
+    //            }
+    //            $scope.comment = $scope.visit.comment;
+    //        }
+    //    };
+    //
+    //    $scope.$watch('visit', function () {
+    //        $scope.showVisit();
+    //    });
 });
