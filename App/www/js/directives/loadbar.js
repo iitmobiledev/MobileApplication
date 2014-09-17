10b4f7b6f87f9f501c45c53170c1e7f522ea8368
Алексей Кочев <kochev.alexey@gmail.com>
2014-09-17 13:09:21 +0400

    loadbar fixed in center on page

--- App/www/js/directives/loadbar.js
+++ App/www/js/directives/loadbar.js
@@ -10,18 +10,18 @@ myApp.directive('loadBar', function () {
     return {
         restrict: 'E',
         replace: true,
-        template: '<div class="loading-image"><img style="position:absolute;left:50%; margin-left: -32px;top: 50%;margin-top: -32px;z-index: 1;" src="images/loading2.gif"><div style="height:100%;width:100%;position:fixed;opacity:0.5;background:white;z-index: 1;top: 0%;"></div></div>',
+        template: '<div class="loading-image"><img style="position:fixed;top:calc(50%);left:calc(50%); margin-left: -32px;margin-top: -32px;z-index: 1;" src="images/loading2.gif"><div style="height:100%;width:100%;position:fixed;opacity:0.5;background:white;z-index: 1;top: 0%;"></div></div>',
         link: function (scope, element, attrs) {
             /*
              *wath, следящий за изменением параметра loading в контроллере
              */
             scope.$watch(attrs.paramName, function (newValue) {
                 if (newValue) {
-//                    $(".loading-image").fadeIn("fast");
-                    $(".loading-image").show();
+                    //                    $(".loading-image").fadeIn("fast");
+                                        $(".loading-image").show();
                 } else {
-//                    $(".loading-image").fadeOut("fast");
-                    $(".loading-image").hide();
+                    //                    $(".loading-image").fadeOut("fast");
+                                        $(".loading-image").hide();
                 }
             }, true);
 
