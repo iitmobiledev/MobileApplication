a2f4c31c7a20c2aa7e045ed40ecb32c52df83cae
Алексей Кочев <kochev.alexey@gmail.com>
2014-09-18 17:13:35 +0600

    small design changes

--- App/www/js/routes.js
+++ App/www/js/routes.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', ['ngRoute','angulartics', 'ngAnimate']);  //,'angulartics'      , 'angulartics.google.analytics'
+var myApp = angular.module('myApp', ['ngRoute','ngAnimate']);  //,'angulartics'      , 'angulartics.google.analytics'
 var flag = true;
 
 
