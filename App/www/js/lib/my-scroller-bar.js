08e3b05da77aa1e5431fc04245d81068a46e26c8
Алексей Кочев <kochev.alexey@gmail.com>
2014-10-06 09:24:48 +0600

    plugin code

--- App/www/js/lib/my-scroller-bar.js
+++ App/www/js/lib/my-scroller-bar.js
@@ -11,9 +11,58 @@
         var MyScrollerBar = window.MyScrollerBar || {};
 
         MyScrollerBar = (function () {
-                function MyScrollerBar(element, settings) {}
+                function MyScrollerBar(element, settings) {
+                    this.defaults = {
+
+                    };
+
+                    this.initials = {
+                        posX: null,
+                        posY: null,
+                        top: null,
+                        screenHeight: null,
+                        percentOfScroll: null,
+                        height: null,
+                    };
+
+                    $.extend(this, this.initials);
+
+                    this.$scrollerBar = $(element);
+
+                    this.options = $.extend({}, this.defaults, settings);
+
+                    this.initEvents();
+                }
                 return MyScrollerBar;
             }
             ());
+
+
+        MyScrollerBar.prototype.initEvents = function () {
+            this.$scroller.on('scroll down', {
+                action: 'scrollDown'
+            }, this.scrollHandler);
+            this.$scroller.on('scroll up', {
+                action: 'scrollUp'
+            }, this.scrollHandler);
+        };
+
+
+        MyScrollerBar.prototype.scrollHandler = function (event) {
+            switch (event.data.action) {
+            case 'scrollDown':
+                break;
+
+            case 'scrollUp':
+                break;
+
+            }
+        };
+
+        MyScrollerBar.prototype.setTranslatePosition = function (position) {
+            this.$scroller.css({
+                'top': position + 'px'
+            });
+        };
 
     }));
